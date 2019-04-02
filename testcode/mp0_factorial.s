30e244e006b8b3f1de60f5e10fa5cf78af9c2937
Vismayak Mohanarajan <mohanar2@linux-a3.ews.illinois.edu>
2019-04-01 21:51:45 -0500

    testfactrotial

--- testcode/mp0_factorial.s
+++ testcode/mp0_factorial.s
@@ -1,47 +1,33 @@
-mp0_factorial.s:
+riscv_mp0test.s:
 .align 4
 .section .text
 .globl _start
 
-#Program to calculate fact(n). Final result is in X2. n is stored in the label number
 _start:
+	lw  x1,factorial
+	addi x1,x1, 1
+	add x2,x0, 1	#start
+	add x3,x0, x0	  #zero
+	add x4,x0, x0   #result
+	add x5,x0, x0	#counter
 
-  #x1: counter for factorial
-  #x2: product
-  #x3: number
-  #x4: counter for mult
-  #x5: temp product
+loop:
+	addi x5,x5,1
+	bgeu x5,x1,end
+	addi x3,x0, 1
 
-  #X1 goes from 1->n, each time multiplying the product (X2) with X1. The multiplication
-  #is carried out using X4 and X5 as temporary registers
+multiply:
+	add x4,x4,x2
+	addi x3,x3,1
+	bltu x3,x5, multiply
+	addi x2,x0, 0 
+	addi x2,x4,0
+	beq x0, x0, loop
 
-
-  lw x3, number #X3 <= n
-  lui x2, 1
-  srli x2, x2, 12 #X2 <= 1
-  addi x1, x2, 0  #X1 <= 1
-
-#main loop for factorial
-loop1:
-  andi x4, x4, 0  #X4 and X5 <= 0
-  andi x5, x5, 0
-
-#loop to multiply X2 with X1
-multloop:
-
-  add x5, x5, x2 #X5 <= X5+X2
-  addi x4, x4, 1  #X4 <= X4 + 1
-  bne x4, x1, multloop  #if X4 == X1, we are done, else repeat
-  #multloop done
-
-  addi x2, x5, 0  #X2 <= X5
-  addi x1, x1, 1  #X1 + 1
-  bge x3, x1, loop1 #if X1 is n, we are done, else repeat
-  #loop1 done
-
-#infinite loop
-halt:
-  beq x1, x1, halt
+end:
+	 beq x0, x0, end
 
 .section .rodata
-number: .word 0x00000005
+
+factorial:        .word 0x05
+
