6c8786e827706eff9f09656bccde5115f1b2dc7d
Utsav Kawrani <kawrani2@illinois.edu>
2019-04-01 22:07:59 -0500

    forwarding works

--- testcode/forward_test.s
+++ testcode/forward_test.s
@@ -5,20 +5,17 @@ riscv_mp0test.s:
 
 _start:
 
-andi x1,x1,$0
-addi x1,x1,$5
+andi x1,x1,0
+addi x1,x1,5
 
-andi x3,x3,$0
-addi x3,x3,$2
+andi x3,x3,0
+addi x3,x3,2
 
-andi x1,x1,$0
-addi x1,x1,$5
+andi x5,x5,0
+addi x5,x5,2
 
-andi x5,x5,$0
-addi x5,x5,$2
-
-andi x6,x6,$0
-addi x6,x6,$8
+andi x6,x6,0
+addi x6,x6,8
 
 
 sub   x2, x1,x3 #3
@@ -30,5 +27,3 @@ end:
 	 beq x0, x0, end
 
 .section .rodata
-
-
--- testcode/mp0_factorial.s
+++ testcode/mp0_factorial.s
@@ -4,7 +4,7 @@ riscv_mp0test.s:
 .globl _start
 
 _start:
-	lw  x1,factorial
+	lw  x1,%lo(factorial)(x0)
 	addi x1,x1, 1
 	add x2,x0, 1	#start
 	add x3,x0, x0	  #zero
@@ -14,20 +14,43 @@ _start:
 loop:
 	addi x5,x5,1
 	bgeu x5,x1,end
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
 	addi x3,x0, 1
 
 multiply:
 	add x4,x4,x2
 	addi x3,x3,1
 	bltu x3,x5, multiply
-	addi x2,x0, 0 
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+	addi x2,x0, 0
 	addi x2,x4,0
 	beq x0, x0, loop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
 
 end:
 	 beq x0, x0, end
+     nop
+     nop
+     nop
+     nop
+     nop
+     nop
 
 .section .rodata
 
 factorial:        .word 0x05
-
