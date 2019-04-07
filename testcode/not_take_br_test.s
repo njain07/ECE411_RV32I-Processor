48fc4ff95fd3b830f3cc5524358ba6ec6c31bb4c
Vismayak Mohanarajan <mohanar2@linux-a3.ews.illinois.edu>
2019-04-07 09:26:13 -0500

    test case to address branching issue added

---
+++ testcode/not_take_br_test.s
@@ -0,0 +1,26 @@
+#  mp3-cp3.s version 1.0
+.align 4
+.section .text
+.globl _start
+_start:
+
+add x1, x0, x0
+addi x1,x1,1
+
+add x2, x0, x0
+addi x2,x2,3
+
+
+good:
+  lw x7, GOOD
+  beq x0,x0,good
+
+bad:
+  lw x7,BAD
+  beq x0,x0,bad
+
+  .section .rodata
+  .balign 256
+
+  BAD:    		.word 0x00BADBAD
+  GOOD:	.word 0x600D600D
